6e3e057208224c801d39801a5a67760970cde2b7
Zohar Arad <zohar@zohararad.com>
2012-08-16 15:38:32 +0300

    added support for views with jst.jade extensions

--- lib/jade.rb
+++ lib/jade.rb
@@ -6,3 +6,4 @@ require 'jade/configuration'
 require 'jade/engine' if defined?(::Rails)
 
 ActionView::Template.register_template_handler :jade, Jade::Renderer
+ActionView::Template.register_template_handler 'jst.jade', Jade::Renderer
