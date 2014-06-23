886ec41331009692aaf66fc8cd6150f6de79cea0
Zohar Arad <zohar@zohararad.com>
2014-06-23 07:43:37 +0300

    run Jader config on inclusion

--- lib/jader.rb
+++ lib/jader.rb
@@ -8,3 +8,5 @@ require 'jader/engine' if defined?(::Rails)
 
 ActionView::Template.register_template_handler :jade, Jader::Renderer
 ActionView::Template.register_template_handler 'jst.jade', Jader::Renderer
+
+Jader.configure do |config|; end
