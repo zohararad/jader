afdea6d0a1f4c533063df3ff803b44a4c8b43ed2
Zohar Arad <zohar@zohararad.com>
2014-06-23 07:19:41 +0300

    Append Jader views path to app.config instead of using a controller before filter

--- lib/jader/engine.rb
+++ lib/jader/engine.rb
@@ -9,11 +9,7 @@ module Jader
 
     initializer 'jader.prepend_views_path', :after => :add_view_paths do |app|
       next if Jader::configuration.nil? or Jader::configuration.views_path.nil?
-      ActionController::Base.class_eval do
-        before_filter do |controller|
-          prepend_view_path Jader::configuration.views_path
-        end
-      end
+      app.config.paths['app/views'].unshift(Jader::configuration.views_path)
     end
 
   end
