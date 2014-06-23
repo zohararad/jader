2dd49c437da9e4f60f3417630ce113197fdc0485
Zohar Arad <zohar@zohararad.com>
2014-06-23 07:43:54 +0300

    update example of config app

--- spec/dummy/config/initializers/jader.rb
+++ spec/dummy/config/initializers/jader.rb
@@ -2,4 +2,5 @@ Jader.configure do |config|
   config.mixins_path = Rails.root.join('app','assets','javascripts','mixins')
   config.views_path = Rails.root.join('app','assets','javascripts','views')
   config.includes << IO.read(Rails.root.join('app','assets','javascripts','includes','util.js'))
+  config.prepend_view_path = true
 end
